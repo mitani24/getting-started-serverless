fad41d9bcbdee20ed4ec8d5e267debb9dd4060f9
mitani24 <h.mitani@ashita-team.com>
2021-07-11 12:59:08 +0900

    initial commit

---
+++ handler.py
@@ -0,0 +1,19 @@
+import boto3
+
+dynamodb = boto3.resource('dynamodb')
+table    = dynamodb.Table('demo-sls-person')
+
+def get_person(id):
+    response = table.get_item(
+            Key={
+                 'person_id': id
+            }
+        )
+    return response['Item']
+
+def get_persons():
+    response = table.scan()
+    return response['Items']
+
+def hello(event, context):
+    return get_persons() if event['person_id'] == '' else get_person(event['person_id'])
